3511327d1636e15beb8e06cf48b39a6732e55e2c
宋健 <2582231611@qq.com>
2023-06-01 10:28:20 +0800

    项目初始化

---
+++ src/main/java/tech/songjian/train/TrainApplication.java
@@ -0,0 +1,12 @@
+package tech.songjian.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainApplication.class, args);
+    }
+}
---
+++ src/test/java/tech/songjian/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.songjian.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
