a2ea4f8887766eb1940a0d4bb7cd85a4abc05dcc
宋健 <2582231611@qq.com>
2023-06-01 16:31:26 +0800

    增加公共模块common，用于防止公共的类，如工具类、拦截器、AOP、枚举、常量、公共配置

--- common/src/main/java/tech/songjian/train/common/aspect/LogAspect.java
+++ common/src/main/java/tech/songjian/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package tech.songjian.train.member.aspect;
+package tech.songjian.train.common.aspect;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.support.spring.PropertyPreFilters;
