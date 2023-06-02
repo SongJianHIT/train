74b2b41227cfbb4bfb2d67d3e7ace590b3080cb4
宋健 <2582231611@qq.com>
2023-06-02 20:06:50 +0800

    增加乘客类型枚举

---
+++ member/src/main/java/tech/songjian/train/member/enums/PassengerTypeEnum.java
@@ -0,0 +1,52 @@
+package tech.songjian.train.member.enums;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 枚举类，用于区分乘客类别：成人、儿童、学生
+ */
+public enum PassengerTypeEnum {
+
+    ADULT("1", "成人"),
+    CHILD("2", "儿童"),
+    STUDENT("3", "学生");
+
+    private String code;
+
+    private String desc;
+
+    PassengerTypeEnum(String code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public static List<HashMap<String,String>> getEnumList() {
+        List<HashMap<String, String>> list = new ArrayList<>();
+        for (PassengerTypeEnum anEnum : EnumSet.allOf(PassengerTypeEnum.class)) {
+            HashMap<String, String> map = new HashMap<>();
+            map.put("code",anEnum.code);
+            map.put("desc",anEnum.desc);
+            list.add(map);
+        }
+        return list;
+    }
+}
