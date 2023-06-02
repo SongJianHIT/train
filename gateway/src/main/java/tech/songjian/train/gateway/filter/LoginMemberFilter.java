52a4670cb9c349f4d90c5cd5985d8ff9ad7cd138
宋健 <2582231611@qq.com>
2023-06-02 20:03:01 +0800

    增加乘客表，生成持久层代码

--- gateway/src/main/java/tech/songjian/train/gateway/filter/LoginMemberFilter.java
+++ gateway/src/main/java/tech/songjian/train/gateway/filter/LoginMemberFilter.java
@@ -12,6 +12,9 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 import tech.songjian.train.gateway.utils.JwtUtil;
 
+/**
+ * @author SongJian
+ */
 @Component
 public class LoginMemberFilter implements Ordered, GlobalFilter {
 
@@ -31,7 +34,8 @@ public class LoginMemberFilter implements Ordered, GlobalFilter {
         } else {
             LOG.info("需要登录验证：{}", path);
         }
-        // 获取header的token参数
+
+        // 获取 header 的 token 参数
         String token = exchange.getRequest().getHeaders().getFirst("token");
         LOG.info("会员登录验证开始，token：{}", token);
         if (token == null || token.isEmpty()) {
@@ -40,7 +44,7 @@ public class LoginMemberFilter implements Ordered, GlobalFilter {
             return exchange.getResponse().setComplete();
         }
 
-        // 校验token是否有效，包括token是否被改过，是否过期
+        // 校验token是否有效，包括 token 是否被改过，是否过期
         boolean validate = JwtUtil.validate(token);
         if (validate) {
             LOG.info("token有效，放行该请求");
---
+++ member/src/main/java/tech/songjian/train/member/mapper/PassengerMapper.java
@@ -0,0 +1,30 @@
+package tech.songjian.train.member.mapper;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import tech.songjian.train.member.domain.Passenger;
+import tech.songjian.train.member.domain.PassengerExample;
+
+public interface PassengerMapper {
+    long countByExample(PassengerExample example);
+
+    int deleteByExample(PassengerExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Passenger record);
+
+    int insertSelective(Passenger record);
+
+    List<Passenger> selectByExample(PassengerExample example);
+
+    Passenger selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Passenger record, @Param("example") PassengerExample example);
+
+    int updateByExample(@Param("record") Passenger record, @Param("example") PassengerExample example);
+
+    int updateByPrimaryKeySelective(Passenger record);
+
+    int updateByPrimaryKey(Passenger record);
+}
