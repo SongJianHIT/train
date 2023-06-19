0bc5dc4cb8e1fec143ee2028bc54beeedda31b27
SongJian_HIT <2582231611@qq.com>
2023-06-19 13:23:56 +0800

    修改拦截器路径

--- member/src/main/java/tech/songjian/train/member/config/SpringMvcConfig.java
+++ member/src/main/java/tech/songjian/train/member/config/SpringMvcConfig.java
@@ -26,9 +26,9 @@ public class SpringMvcConfig implements WebMvcConfigurer {
        registry.addInterceptor(memberInterceptor)
                .addPathPatterns("/**")
                .excludePathPatterns(
-                       "/member/hello",
-                       "/member/member/send-code",
-                       "/member/member/login"
+                       "/hello",
+                       "/member/send-code",
+                       "/member/login"
                );
    }
 }
